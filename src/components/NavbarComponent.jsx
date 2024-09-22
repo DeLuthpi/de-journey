e6277281b0a4e5a7763501923d732cfd78da4035
DeLuthpi <dedeluthpi006@gmail.com>
2024-09-22 12:20:10 +0700

    fix warning hydration

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button, Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, NavbarMenuToggle, Avatar } from "@nextui-org/react";
 import React, { useEffect, useRef } from "react";
 import { getCookie, deleteCookie } from "cookies-next";
@@ -10,6 +12,11 @@ const NavbarComponent = () => {
 	const [isAdmin, setIsAdmin] = React.useState(true);
 	const [menuList, setMenuList] = React.useState(listMenu);
 	const [theme, setTheme] = React.useState("light");
+	const [isClient, setIsClient] = React.useState(false);
+
+	useEffect(() => {
+		setIsClient(true);
+	}, []);
 
 	const handleTheme = () => {
 		setTheme(theme === "light" ? "dark" : "light");
@@ -32,104 +39,108 @@ const NavbarComponent = () => {
 
 	useEffect(() => {
 		const handleOutSideClick = (event) => {
-			if (!dropdownRef.current?.contains(event.target)) {
+			if (!dropdownRef?.current?.contains(event?.target)) {
 				setIsMenuOpen(false);
 			}
 		};
 
-		window.addEventListener("mousedown", handleOutSideClick);
+		window?.addEventListener("mousedown", handleOutSideClick);
 
 		return () => {
-			window.removeEventListener("mousedown", handleOutSideClick);
+			window?.removeEventListener("mousedown", handleOutSideClick);
 		};
 	}, [dropdownRef]);
 
 	return (
-		<Navbar className="w-[94%] md:w-[97%] mx-auto mt-4 rounded-lg shadow-2xl justify-center" onMenuOpenChange={() => setIsMenuOpen(!isMenuOpen)}>
-			<NavbarBrand>
-				<img className="w-auto h-auto dark:invert max-h-11" src={logoImage} alt="logo images" width={160} height={45} />
-			</NavbarBrand>
-			<NavbarContent className="hidden gap-12 lg:flex" justify="center">
-				{menuList.map((menu) => (
-					<NavbarItem key={menu?.text}>
-						<Link color="foreground" href={menu?.link} className="hover:text-[#fa8443]">
-							{menu?.name}
-						</Link>
-					</NavbarItem>
-				))}
-			</NavbarContent>
-			<NavbarContent as="div" justify="end">
-				<Dropdown placement="bottom-end right" className={`${isLoggedIn ? "bg-[#f9fafc] mt-2" : "bg-[#f9fafc]"}`}>
-					<DropdownTrigger>{isLoggedIn ? <Avatar isBordered as="button" className="transition-transform" src="https://i.pravatar.cc/150?u=a042581f4e29026704d" /> : <NavbarMenuToggle data-open={isMenuOpen} aria-label={isMenuOpen ? "Open menu" : "Close menu"} className="transition-transform lg:hidden" />}</DropdownTrigger>
-					<DropdownMenu variant="flat" ref={dropdownRef}>
-						{menuList.map((menu) => (
-							<DropdownItem key={menu?.text} textValue={menu?.text} className="flex lg:hidden">
-								<Link href={menu?.link} className="text-gray-700">
+		<>
+			{isClient && (
+				<Navbar className="w-[94%] md:w-[97%] mx-auto mt-4 rounded-lg shadow-2xl justify-center" onMenuOpenChange={() => setIsMenuOpen(!isMenuOpen)}>
+					<NavbarBrand>
+						<img className="w-auto h-auto dark:invert max-h-11" src={logoImage} alt="logo images" width={160} height={45} />
+					</NavbarBrand>
+					<NavbarContent className="hidden gap-12 lg:flex" justify="center">
+						{menuList?.map((menu) => (
+							<NavbarItem key={menu?.text}>
+								<Link color="foreground" href={menu?.link} className="hover:text-[#fa8443]">
 									{menu?.name}
 								</Link>
-							</DropdownItem>
+							</NavbarItem>
 						))}
+					</NavbarContent>
+					<NavbarContent as="div" justify="end">
+						<Dropdown placement="bottom-end right" className={`${isLoggedIn ? "bg-[#f9fafc] mt-2" : "bg-[#f9fafc]"}`}>
+							<DropdownTrigger>{isLoggedIn ? <Avatar isBordered as="button" className="transition-transform" src="https://i.pravatar.cc/150?u=a042581f4e29026704d" /> : <NavbarMenuToggle data-open={isMenuOpen} aria-label={isMenuOpen ? "Open menu" : "Close menu"} className="transition-transform lg:hidden" />}</DropdownTrigger>
+							<DropdownMenu aria-label="Profile Actions" variant="flat" ref={dropdownRef}>
+								{menuList.map((menu) => (
+									<DropdownItem key={menu?.text} textValue={menu?.text} className="flex lg:hidden">
+										<Link href={menu?.link} className="text-gray-700">
+											{menu?.name}
+										</Link>
+									</DropdownItem>
+								))}
 
-						<DropdownItem key="theme" textValue="theme" showDivider={isLoggedIn ? false : true}>
-							<Link onClick={handleTheme} className="text-gray-700">
-								Mode
-							</Link>
-						</DropdownItem>
+								<DropdownItem key="theme" textValue="theme" showDivider={isLoggedIn ? false : true}>
+									<Link onClick={handleTheme} className="text-gray-700">
+										Mode
+									</Link>
+								</DropdownItem>
 
-						{isLoggedIn && (
-							<DropdownItem key="profile" textValue="profile" showDivider={isAdmin ? false : true}>
-								<Link href="/profile" className="text-gray-700">
-									Profile
-								</Link>
-							</DropdownItem>
-						)}
+								{isLoggedIn && (
+									<DropdownItem key="profile" textValue="profile" showDivider={isAdmin ? false : true}>
+										<Link href="/profile" className="text-gray-700">
+											Profile
+										</Link>
+									</DropdownItem>
+								)}
 
-						{isLoggedIn && isAdmin && (
-							<DropdownItem key="dashboard" textValue="dashboard" showDivider={isAdmin ? true : false}>
-								<Link href="/dashboard" className="text-gray-700">
-									Dashboard
-								</Link>
-							</DropdownItem>
-						)}
+								{isLoggedIn && isAdmin && (
+									<DropdownItem key="dashboard" textValue="dashboard" showDivider={isAdmin ? true : false}>
+										<Link href="/dashboard" className="text-gray-700">
+											Dashboard
+										</Link>
+									</DropdownItem>
+								)}
 
-						{isLoggedIn ? (
-							<DropdownItem key="logout" color="danger" textValue="logout">
-								<Link onClick={handleLogout} className="text-gray-700">
-									Logout
-								</Link>
-							</DropdownItem>
-						) : (
-							<DropdownItem key="login" textValue="login">
-								<Link href="/login" className="text-gray-700">
-									Login
-								</Link>
-							</DropdownItem>
+								{isLoggedIn ? (
+									<DropdownItem key="logout" color="danger" textValue="logout">
+										<Link onClick={handleLogout} className="text-danger">
+											Logout
+										</Link>
+									</DropdownItem>
+								) : (
+									<DropdownItem key="login" textValue="login">
+										<Link href="/login" className="text-gray-700">
+											Login
+										</Link>
+									</DropdownItem>
+								)}
+
+								{isLoggedIn === false && (
+									<DropdownItem key="register" textValue="register">
+										<Link href="/register" className="text-gray-700">
+											Register
+										</Link>
+									</DropdownItem>
+								)}
+							</DropdownMenu>
+						</Dropdown>
+						{isLoggedIn === false && (
+							<NavbarItem className="hidden lg:flex">
+								<Link href="/login">Login</Link>
+							</NavbarItem>
 						)}
 
 						{isLoggedIn === false && (
-							<DropdownItem key="register" textValue="register">
-								<Link href="/register" className="text-gray-700">
+							<NavbarItem className="hidden lg:flex">
+								<Button as={Link} color="primary" className="text-white rounded-full bg-primary" href="/register" variant="ghost">
 									Register
-								</Link>
-							</DropdownItem>
+								</Button>
+							</NavbarItem>
 						)}
-					</DropdownMenu>
-				</Dropdown>
-				{isLoggedIn === false && (
-					<NavbarItem className="hidden lg:flex">
-						<Link href="/login">Login</Link>
-					</NavbarItem>
-				)}
-
-				{isLoggedIn === false && (
-					<NavbarItem className="hidden lg:flex">
-						<Button as={Link} color="primary" className="text-white rounded-full bg-primary" href="/register" variant="ghost">
-							Register
-						</Button>
-					</NavbarItem>
-				)}
-			</NavbarContent>
-		</Navbar>
+					</NavbarContent>
+				</Navbar>
+			)}
+		</>
 	);
 };
 
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -6,9 +6,19 @@ export default function App({ Component, pageProps }) {
 	return (
 		<>
 			<Head>
-				<title>De Journey Vacations</title>
+				<meta charset="UTF-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<meta name="description" content="De Journey Vacations" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" type="image/svg+xml" href="/favicon.ico" />
+				<title>De Journey Vacations</title>
+				<meta http-equiv="x-ua-compatible" content="ie=edge" />
+				<meta name="author" content="De Journey Vacations" />
+				<meta name="keywords" content="De Journey Vacations" />
+				<meta name="robots" content="INDEX,FOLLOW" />
+				<meta name="theme-color" content="#ffffff" /> <link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
 			</Head>
 			<Toaster
 				containerClassName="mt-20"
