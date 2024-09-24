04f1b09a1a0064ab26e6a79a0c2b6f72f3f5ada2
DeLuthpi <dedeluthpi006@gmail.com>
2024-09-25 06:29:49 +0700

    install reduxjs toolkit and styling sidebar menu when active

--- src/components/SidebarAdmin.jsx
+++ src/components/SidebarAdmin.jsx
@@ -20,19 +20,19 @@ const SidebarAdmin = () => {
 				<ul className="flex flex-col pl-0 mb-0 list-none">
 					{dashboardMenu?.map((menu) => (
 						<li className="mt-0.5 w-full" key={menu?.text}>
-							<Link href={menu?.path} className={`${pathname === menu?.path ? "bg-[#023047] font-normal" : "font-normal"} sidenav-menu ease-soft-in-out text-sm py-2.7 xl:shadow-soft-xl my-0 mx-4 flex items-center whitespace-nowrap rounded-lg p-2 transition-all duration-300 dark:text-white dark:opacity-80 shadow-none`}>
-								<div className={`${pathname === menu?.path ? "text-[#023047] bg-gradient-to-tl from-primary to-[#8ECAE6]" : "text-[#8ECAE6] bg-[#023047]"} sidenav-icon flex items-center justify-center w-8 h-8 p-0 mr-2 text-center bg-center rounded-lg fill-current stroke-none shadow-soft-sm`}>{menu?.icon}</div>
-								<span className={`${pathname === menu?.path ? "text-white" : "text-[#023047]"} sidenav-text ml-1 text-white duration-300 opacity-100 pointer-events-none ease-soft`}>{menu?.name}</span>
+							<Link href={menu?.path} className={`sidenav-menu ${pathname === menu?.path ? "active" : ""} ease-soft-in-out py-2.7 xl:shadow-soft-xl`}>
+								<div className="sidenav-icon shadow-soft-sm">{menu?.icon}</div>
+								<span className="sidenav-text ease-soft">{menu?.name}</span>
 							</Link>
 						</li>
 					))}
 					<hr className="h-px mt-1 bg-transparent border-0 border-t-0 opacity-25 bg-gradient-to-r from-transparent via-black/40 to-transparent dark:bg-gradient-to-r dark:from-transparent dark:via-white dark:to-transparent" />
 					<li className="mt-0.5 w-full">
-						<Link href="/" className="ease-soft-in-out text-sm py-2.7 xl:shadow-soft-xl my-0 mx-4 flex items-center whitespace-nowrap rounded-lg p-2 font-normal transition-all dark:text-white dark:opacity-80 shadow-none">
-							<div className="flex items-center justify-center w-8 h-8 p-0 mr-2 text-center text-[#8ECAE6] bg-[#023047] bg-center rounded-lg fill-current stroke-none shadow-soft-sm">
+						<Link href="/" className="sidenav-menu ease-soft-in-out py-2.7 xl:shadow-soft-xl">
+							<div className="sidenav-icon shadow-soft-sm">
 								<LiaGlobeAmericasSolid className="w-5 h-5" />
 							</div>
-							<span className="ml-1 text-[#023047] duration-300 opacity-100 pointer-events-none ease-soft">Page Preview</span>
+							<span className="sidenav-text ease-soft">Page Preview</span>
 						</Link>
 					</li>
 				</ul>
