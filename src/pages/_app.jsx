7f812420b798094640e9878b03cc563d97fa43b4
DeLuthpi <dedeluthpi006@gmail.com>
2024-09-26 22:33:41 +0700

    import name and fix warning invalid dom property

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,6 +4,7 @@ import { Toaster } from "react-hot-toast";
 import { usePathname } from "next/navigation";
 import { Provider } from "react-redux";
 import store from "@/redux/store";
+import { logoName } from "@/helpers/const";
 
 export default function App({ Component, pageProps }) {
 	const pathname = usePathname();
@@ -11,19 +12,19 @@ export default function App({ Component, pageProps }) {
 	return (
 		<Provider store={store}>
 			<Head>
-				<meta charset="UTF-8" />
+				<meta charSet="UTF-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-				<meta name="description" content="De Journey Vacations" />
+				<meta name="description" content={logoName} />
 				<link rel="icon" type="image/svg+xml" href="/favicon.ico" />
-				<title>De Journey Vacations</title>
-				<meta http-equiv="x-ua-compatible" content="ie=edge" />
-				<meta name="author" content="De Journey Vacations" />
-				<meta name="keywords" content="De Journey Vacations" />
+				<title>{logoName}</title>
+				<meta httpEquiv="x-ua-compatible" content="ie=edge" />
+				<meta name="author" content={logoName} />
+				<meta name="keywords" content={logoName} />
 				<meta name="robots" content="INDEX,FOLLOW" />
 				<meta name="theme-color" content="#ffffff" /> <link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
 			</Head>
 			<Toaster
 				containerClassName="mt-20"
